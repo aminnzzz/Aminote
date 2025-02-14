4ba3bddb8f99a342ccae50462bd3c456844cbd74
Amin Nazemzadeh <amin.nazemzadeh96@gmail.com>
2025-02-14 00:00:35 -0500

    36

--- Aminote/DataController.swift
+++ Aminote/DataController.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreData
+import StoreKit
 import SwiftUI
 
 enum SortType: String {
@@ -42,6 +43,9 @@ class DataController: ObservableObject {
 
     /// The UserDefault's suite where we're saving user data.
     let defaults: UserDefaults
+
+    /// The store kit products we loaded from the store
+    @Published var products = [Product]()
 
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
@@ -339,6 +343,9 @@ class DataController: ObservableObject {
             let request = Tag.fetchRequest()
             let awardCount = count(for: request)
             return awardCount >= award.value
+
+        case "unlock":
+            return fullVersionUnlocked
 
         default:
 //            fatalError("Unknown award criterion: \(award.criterion)")
--- Aminote/Issues/ContentView.swift
+++ Aminote/Issues/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.requestReview) var requestReview
     @StateObject var viewModel: ViewModel
 
     init(dataController: DataController) {
@@ -39,6 +40,13 @@ struct ContentView: View {
             }
         }
         .toolbar(content: ContentViewToolbar.init)
+        .onAppear(perform: askForReview)
+    }
+
+    func askForReview() {
+        if viewModel.shouldRequestReview {
+            requestReview()
+        }
     }
 }
 
